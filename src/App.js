4891018560f520da50b90ebf4a1d911ea2ae26db
masterlaver13 <42409601+masterlaver13@users.noreply.github.com>
2021-02-01 20:51:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './style.css';
 import './faq.css';
-import React, { useState} from 'react';
+import React, {useState} from 'react';
 import Question from './question'
-
 
 function App() {
 
@@ -12,41 +11,31 @@ function App() {
     {
       "question": 'How many team members can I invite?',
       "answer": "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
-    },
-    {
+    }, {
       "question": 'What is the maximum file upload size?',
       "answer": "No more than 2GB. All files in your account must fit your allotted storage space."
-    },
-    {
+    }, {
       "question": 'How do I reset my password?',
       "answer": "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
-    },
-    {
+    }, {
       "question": 'Can I cancel my subscription?',
       "answer": "Yes! Send us a message and we’ll process your request no questions asked."
-    },
-    {
+    }, {
       "question": 'Do you provide additional support?',
       "answer": "Chat and email support is available 24/7. Phone lines are open during normal business hours."
     }
   ]);
 
-  return (
+  return (<div id="wrapper">
+    <h1>FAQ</h1>
 
-    <div id="wrapper">
-      <h1>FAQ</h1>
+    <Question question={questions[0]["question"]} answer={questions[0]["answer"]}/>
+    <Question question={questions[1]["question"]} answer={questions[1]["answer"]}/>
+    <Question question={questions[2]["question"]} answer={questions[2]["answer"]}/>
+    <Question question={questions[3]["question"]} answer={questions[3]["answer"]}/>
+    <Question question={questions[4]["question"]} answer={questions[4]["answer"]}/>
 
-
-        <Question s={() =>{setCurrIndex(currIndex+1)}} question={questions[0]["question"]} answer={questions[0]["answer"]} />
-        <Question s={() =>{setCurrIndex(currIndex+1)}} question={questions[1]["question"]} answer={questions[1]["answer"]} />
-        <Question s={() =>{setCurrIndex(currIndex+1)}} question={questions[2]["question"]} answer={questions[2]["answer"]} />
-        <Question s={() =>{setCurrIndex(currIndex+1)}} question={questions[3]["question"]} answer={questions[3]["answer"]} />
-        <Question s={() =>{setCurrIndex(currIndex+1)}} question={questions[4]["question"]} answer={questions[4]["answer"]} />
-
-  </div>
-
-
-  );
+  </div>);
 }
 
 export default App;
