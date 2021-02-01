a8656385b23ed4bb10cd86875af646abb6551332
masterlaver13 <42409601+masterlaver13@users.noreply.github.com>
2021-02-01 23:07:50 +0100

    JS

--- src/App.js
+++ src/App.js
@@ -5,37 +5,54 @@ import Question from './question'
 
 function App() {
 
-  const [currIndex, setCurrIndex] = useState(0);
-
   const [questions, setQuestion] = useState([
     {
-      "question": 'How many team members can I invite?',
-      "answer": "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
-    }, {
-      "question": 'What is the maximum file upload size?',
-      "answer": "No more than 2GB. All files in your account must fit your allotted storage space."
-    }, {
-      "question": 'How do I reset my password?',
-      "answer": "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
-    }, {
-      "question": 'Can I cancel my subscription?',
-      "answer": "Yes! Send us a message and we’ll process your request no questions asked."
-    }, {
-      "question": 'Do you provide additional support?',
-      "answer": "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+      question: 'How many team members can I invite?',
+      answer: "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+      open: false
+    },
+    {
+      question: 'What is the maximum file upload size?',
+      answer: "No more than 2GB. All files in your account must fit your allotted storage space.",
+      open: false
+    },
+    {
+      question: 'How do I reset my password?',
+      answer: "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+      open: false
+    },
+    {
+      question: 'Can I cancel my subscription?',
+      answer: "Yes! Send us a message and we’ll process your request no questions asked.",
+      open: false
+    },
+    {
+      question: 'Do you provide additional support?',
+      answer: "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+      open: false
     }
   ]);
 
-  return (<div id="wrapper">
-    <h1>FAQ</h1>
+  const toggleFAQ = index =>{
+    setQuestion(questions.map((question, i) => {
+      if(i===index){question.open = !question.open;}
+      else {question.open = false;}
 
-    <Question question={questions[0]["question"]} answer={questions[0]["answer"]}/>
-    <Question question={questions[1]["question"]} answer={questions[1]["answer"]}/>
-    <Question question={questions[2]["question"]} answer={questions[2]["answer"]}/>
-    <Question question={questions[3]["question"]} answer={questions[3]["answer"]}/>
-    <Question question={questions[4]["question"]} answer={questions[4]["answer"]}/>
+      return question;
+    }))
+  }
 
-  </div>);
+
+  return (
+    <div id="wrapper">
+      <h1 id="header">FAQ</h1>
+
+      {questions.map((question, i) => (
+            <Question question={question} index={i} key={i} toggleFAQ={toggleFAQ}/>
+        ))}
+
+    </div>
+  );
 }
 
 export default App;
--- src/question.js
+++ src/question.js
@@ -2,19 +2,17 @@ import React from 'react';
 import './style.css';
 import './faq.css';
 
-export default function Question({question, answer}) {
+export default function Question({question, index, toggleFAQ}) {
 
-  const questionStyle = {};
-  const answerStyle = {};
+  return (
+    <div
+      className={"faq " + (question.open ? 'open': '')}
+      key={index}
+      onClick={() => toggleFAQ(index)}
+    >
+      <div className="question">{question.question}</div>
+        <div className="answer">{question.answer}</div>
 
-  return (< div className = "container" > <div id="question">
-    <a href="#question1">{question}</a>
-    <div className="answer">
-      <p>
-        {answer}
-      </p>
     </div>
-  </div> < /div>
-);
-
+  );
 }
